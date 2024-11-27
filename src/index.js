a23bc1f9d1480e1e4ddcd0d49f4192e4ef606ed7
upasana <bhandariupasana2111@gmail.com>
2024-11-27 07:14:29 +0530

    created route for 'user' and saving data to db

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-const express= require('express');
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user');
 
-const app=express();
-const port= process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json()); //automatically parse json into object
-app.post('/users',(req,res)=>{
-console.log(req.body)
- res.send('testing!!')
+app.post('/users', (req, res) => {
+    const user = new User(req.body);
+
+    user.save().then(() => {
+        res.send(user);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Server is up on port' + port);
 })
